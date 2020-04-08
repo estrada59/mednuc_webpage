799fc727384787063c0177069ec75f59c8be49a7
enrique estrada <estrada59@gmail.com>
2020-04-08 16:57:01 -0500

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,7 @@
                 </div>
 
                 <?php   
-                    //print_r($_ENV);
+                    //print_r($_ENV);  //ssss
                    //echo '<pre>'; print_r($_SERVER); echo '</pre>';
                 ?>
             </div>
